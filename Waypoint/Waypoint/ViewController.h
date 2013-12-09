8822baf4053734bfbba1c981dfca0d658343d1bc
Steven <steven@Stevens-Mac.local>
2013-12-08 20:09:58 -0800

    init project

---
+++ Waypoint/Waypoint/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Waypoint
+//
+//  Created by Steven on 12/6/13.
+//  Copyright (c) 2013 Steven. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
